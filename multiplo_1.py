1c81e17202999e206674c025ceaca260725b4d04
Jacinto Rodriguez <vjrodriguez75@gmail.com>
2022-07-14 22:14:35 -0500

    prirmera version del programa

--- multiplo_1.py
+++ multiplo_1.py
@@ -1,9 +1,19 @@
 # --*-- coding: utf-8 -*-
 
-# Algoritmo: Multiplo de un numero
+# Algoritmo: Multiplo de un numero y suma de esos multiplos
 
 def main():
-    pass
+    #Entradas
+    numero = int(input('Escribe un numero entero: '))
+    rango = int(input('¿hasta donde quieres hallar los multiplos? escribe un entero: '))
+    #Proceso
+    multiplo = []
+    for i in range(numero, rango):
+        if i % numero == 0:
+            multiplo.append(i)
+    #Salida
+    print(f'Los multiplos de {numero} son:\n{multiplo}')
+    print(f'La suma de los multiplos de {numero} es:\n{sum(multiplo)}')
 
 if __name__ == '__main__':
     main()
